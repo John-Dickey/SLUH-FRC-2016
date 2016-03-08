532187b3a1764c3d287450ca77b8cd0371509017
LeSquirrels <john@strategyworking.com>
2016-03-08 16:00:29 -0600

    restart camera server from smartdashboard

--- RpiLeds.py
+++ RpiLeds.py
@@ -1,8 +1,5 @@
-import sys
-import time
+import sys, time, logging, serial, os
 from networktables import NetworkTable
-import serial
-import logging
 logging.basicConfig(level=logging.DEBUG)
 
 NetworkTable.setIpAddress("roborio-5176-frc.local")
@@ -11,9 +8,12 @@ NetworkTable.initialize()
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-sd = NetworkTable.getTable("LEDS")
+sd = NetworkTable.getTable("RPI")
 pattern = sd.getAutoUpdateValue('pattern', 0)
-
+theMoney = sd.getAutoUpdateValue('doTheRestart?', 0)
 while True:
 	ser.write(pattern)
+	if theMoney == 1:
+		os.system("sudo service motion restart")
+		sd.putNumber('doTheRestart?', 0)
 	time.sleep(1)
